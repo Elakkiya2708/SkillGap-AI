fc686b237567187fed124a4af3d2ec67683cbafe
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-20 20:20:10 +0530

    api

--- backend/services/api.js
+++ backend/services/api.js
@@ -13,3 +13,21 @@ export async function analyzeSkills(userSkills, requiredSkills) {
 
   return response.json();
 }
+
+
+export async function uploadResume(file) {
+
+  const formData = new FormData();
+
+  formData.append("file", file);
+
+  const response = await fetch(
+    "http://127.0.0.1:8000/upload-resume",
+    {
+      method: "POST",
+      body: formData
+    }
+  );
+
+  return response.json();
+}

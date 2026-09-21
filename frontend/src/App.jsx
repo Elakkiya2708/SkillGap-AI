efcf1c1c6e5cdefc9d89f9553ce8537f2902efea
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-21 21:25:13 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Analyze from "./pages/Analyze";
 import History from "./pages/History";
@@ -6,6 +6,24 @@ import History from "./pages/History";
 function App() {
   return (
     <BrowserRouter>
+
+      <nav
+        style={{
+          padding: "15px 30px",
+          borderBottom: "1px solid #ddd",
+          display: "flex",
+          gap: "20px"
+        }}
+      >
+        <Link to="/">
+          Analyze
+        </Link>
+
+        <Link to="/history">
+          Analysis History
+        </Link>
+      </nav>
+
       <Routes>
 
         <Route
@@ -19,6 +37,7 @@ function App() {
         />
 
       </Routes>
+
     </BrowserRouter>
   );
 }

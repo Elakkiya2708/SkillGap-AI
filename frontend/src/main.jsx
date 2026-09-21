d3d49c515dd4b612bf7fc542db8287a7b2b7bdcc
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-21 20:39:55 +0530

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'from fastapi.responses 
-import StreamingResponse
-from reportlab.pdfgen 
-import canvas
-
-import io
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
